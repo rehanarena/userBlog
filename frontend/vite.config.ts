edbf25ec28478718422af2a122b10de1208e6fe6
Rehana <nfsrehana@gmail.com>
2025-05-03 11:48:34 +0530

    config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server:{port:5173}
+  build: {
+    outDir: 'dist',      // make it explicit
+  },
+  base: '/',             // ensure your assets load from root
 })
