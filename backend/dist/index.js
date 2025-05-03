6a97b521bc2687d0ddb5e10b7b1115fb7adbb510
Rehana <nfsrehana@gmail.com>
2025-05-03 22:42:23 +0530

    Remove node_modules from repo

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,14 +5,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const mongodb_1 = __importDefault(require("./config/mongodb"));
+const authRoute_1 = __importDefault(require("./routes/authRoute"));
+const userRoute_1 = __importDefault(require("./routes/userRoute"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 //api config
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+dotenv_1.default.config();
+(0, mongodb_1.default)();
 //middlewares
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cookie_parser_1.default)());
+const allowedOrigins = [
+    'https://mini-store.shop',
+    'http://localhost:5173',
+];
+app.use((0, cors_1.default)({
+    origin: allowedOrigins,
+    credentials: true,
+}));
 //api endpoint
 app.get('/api', (req, res) => {
     res.send('Api Working');
 });
+const uploadDir = path_1.default.join(__dirname, "../uploads");
+if (!fs_1.default.existsSync(uploadDir)) {
+    fs_1.default.mkdirSync(uploadDir, { recursive: true });
+}
+app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "../uploads")));
+//Routes
+app.use("/api/auth", authRoute_1.default);
+app.use("/api/user", userRoute_1.default);
 app.listen(port, () => console.log('Server Started', port));
