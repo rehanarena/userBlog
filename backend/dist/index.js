f698dabb6d83f70e645ba81f0d66bbefcc0ddf92
Rehana <nfsrehana@gmail.com>
2025-05-04 12:26:04 +0530

    cors

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -13,24 +13,22 @@ const authRoute_1 = __importDefault(require("./routes/authRoute"));
 const userRoute_1 = __importDefault(require("./routes/userRoute"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
+(0, mongodb_1.default)();
 //api config
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 dotenv_1.default.config();
-(0, mongodb_1.default)();
 //middlewares
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-const isProd = process.env.NODE_ENV === 'PRODUCTION';
-const allowedOrigins = [
-    isProd
-        ? process.env.PROD_CLIENT_LINK
-        : process.env.DEV_CLIENT_LINK,
-];
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: ["http://localhost:5173", "https://user-blog-three.vercel.app"],
+    credentials: true,
+    methods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS']
+}));
 //api endpoint
-app.get('/api', (req, res) => {
-    res.send('Api Working');
+app.get("/api", (req, res) => {
+    res.send("Api Working");
 });
 const uploadDir = path_1.default.join(__dirname, "../uploads");
 if (!fs_1.default.existsSync(uploadDir)) {
@@ -40,4 +38,4 @@ app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "../
 //Routes
 app.use("/api/auth", authRoute_1.default);
 app.use("/api/user", userRoute_1.default);
-app.listen(port, () => console.log('Server Started', port));
+app.listen(port, () => console.log("Server Started", port));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,44 +1,37 @@
 import express from "express";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import 'dotenv/config';
-import cookieParser from 'cookie-parser';
+import cors from "cors";
+import dotenv from "dotenv";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb";
-import authRouter from './routes/authRoute'
+import authRouter from "./routes/authRoute";
 import userRouter from "./routes/userRoute";
 import fs from "fs";
 import path from "path";
 
-
-//api config
-const app = express()
-const port = process.env.PORT || 3000
-
-dotenv.config();  
 connectDB();
 
+//api config
+const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+
 //middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
-
-const isProd = process.env.NODE_ENV === 'PRODUCTION';
-const allowedOrigins = [
-  isProd 
-    ? process.env.PROD_CLIENT_LINK! 
-    : process.env.DEV_CLIENT_LINK!,
-];
-
 app.use(
-  cors(
-    )
+  cors({
+    origin: ["http://localhost:5173", "https://user-blog-three.vercel.app"],
+    credentials: true,
+    methods: ['POST','GET','PUT','DELETE','OPTIONS']
+  })
 );
 
-
 //api endpoint
-app.get('/api',(req,res)=>{
-    res.send('Api Working')
+app.get("/api", (req, res) => {
+  res.send("Api Working");
 });
-
 
 const uploadDir = path.join(__dirname, "../uploads");
 if (!fs.existsSync(uploadDir)) {
@@ -48,7 +41,7 @@ if (!fs.existsSync(uploadDir)) {
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 //Routes
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.listen(port,()=>console.log('Server Started',port));
+app.listen(port, () => console.log("Server Started", port));
